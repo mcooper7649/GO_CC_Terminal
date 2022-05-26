1f18c3fcd5f79e64fcd1d326c70f7b998613fd11
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2022-05-26 15:55:19 -0400

    added backend handlers-api for payment intent

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/fatih/color v1.13.0 // indirect
 	github.com/fsnotify/fsnotify v1.5.4 // indirect
 	github.com/go-chi/chi/v5 v5.0.3 // indirect
+	github.com/go-chi/cors v1.2.1 // indirect
 	github.com/imdario/mergo v0.3.13 // indirect
 	github.com/mattn/go-colorable v0.1.12 // indirect
 	github.com/pelletier/go-toml v1.9.5 // indirect
--- internal/card/card.go
+++ internal/card/card.go
@@ -1,6 +1,9 @@
 package card
 
-import "github.com/stripe/stripe-go/v72"
+import (
+	"github.com/stripe/stripe-go/v72"
+	"github.com/stripe/stripe-go/v72/paymentintent"
+)
 
 type Card struct {
 	Secret   string
@@ -9,18 +12,18 @@ type Card struct {
 }
 
 type Transaction struct {
-	TransactionStatusId int
+	TransactionStatusID int
 	Amount              int
 	Currency            string
-	LastFour            int
+	LastFour            string
 	BankReturnCode      string
 }
 
-func (c *Card) Charge(currency string, amount int)(*stripe.PaymentIntent, string, error){
-	return c.createPaymentIntent(currency, amount)
+func (c *Card) Charge(currency string, amount int) (*stripe.PaymentIntent, string, error) {
+	return c.CreatePaymentIntent(currency, amount)
 }
 
-func (c *Card) createPaymentIntent(currency string, amount int) (*stripe.PaymentIntent, string, error) {
+func (c *Card) CreatePaymentIntent(currency string, amount int) (*stripe.PaymentIntent, string, error) {
 	stripe.Key = c.Secret
 
 	// create a payment intent
@@ -31,7 +34,7 @@ func (c *Card) createPaymentIntent(currency string, amount int) (*stripe.Payment
 
 	//params.AddMetadata("key", "value")
 
-	pi, err := paymentIntent.New(params)
+	pi, err := paymentintent.New(params)
 	if err != nil {
 		msg := ""
 		if stripeErr, ok := err.(*stripe.Error); ok {
@@ -42,17 +45,27 @@ func (c *Card) createPaymentIntent(currency string, amount int) (*stripe.Payment
 	return pi, "", nil
 }
 
-
 func cardErrorMessage(code stripe.ErrorCode) string {
 	var msg = ""
 	switch code {
-	case: stripe.ErrorCodeCardDeclined:
+	case stripe.ErrorCodeCardDeclined:
 		msg = "Your card was declined"
-	case: stripe.ErrorCodeCardExpiredCard:
-		msg: "Your card was expired"
-	default: 
+	case stripe.ErrorCodeExpiredCard:
+		msg = "Your card is expired"
+	case stripe.ErrorCodeIncorrectCVC:
+		msg = "Incorrect CVC code"
+	case stripe.ErrorCodeIncorrectZip:
+		msg = "Incorrect zip/postal code"
+	case stripe.ErrorCodeAmountTooLarge:
+		msg = "The amount is too large to charge to your card"
+	case stripe.ErrorCodeAmountTooSmall:
+		msg = "The amount is too small to charge to your card"
+	case stripe.ErrorCodeBalanceInsufficient:
+		msg = "Insufficient balance"
+	case stripe.ErrorCodePostalCodeInvalid:
+		msg = "Your postal code is invalid"
+	default:
 		msg = "Your card was declined"
 	}
-
-	return msg;
+	return msg
 }
